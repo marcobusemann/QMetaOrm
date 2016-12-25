2950912b494bc549557c93ce2fae1253f4b98fb4
Marco Busemann <marco.busemann@live.com>
2016-12-25 11:10:52 +0100

    First appveyor test

--- scripts/appveyor/msvc2015-qt57.bat
+++ scripts/appveyor/msvc2015-qt57.bat
@@ -1,5 +1,5 @@
 set QTDIR=C:\Qt\5.7\msvc2015
 set PATH=%PATH%;%QTDIR%\bin
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 qmake
 nmake
