fae6c13cf1e0b761bef16485236b4c0c338db1d5
Marco Busemann <marco.busemann@live.com>
2017-02-12 13:56:33 +0100

    Qt versions compatible exception test

--- test/SessionIT/SessionIT.cpp
+++ test/SessionIT/SessionIT.cpp
@@ -10,6 +10,31 @@
 
 using namespace QMetaOrm;
 
+/*
+    Took from qt source for usage in old qt versions
+*/
+#  define VERIFY_EXCEPTION_THROWN(expression, exceptiontype) \
+    do {\
+        QT_TRY {\
+            QT_TRY {\
+                expression;\
+                QTest::qFail("Expected exception of type " #exceptiontype " to be thrown" \
+                             " but no exception caught", __FILE__, __LINE__);\
+                return;\
+            } QT_CATCH (const exceptiontype &) {\
+            }\
+        } QT_CATCH (const std::exception &e) {\
+            QByteArray msg = QByteArray() + "Expected exception of type " #exceptiontype \
+                             " to be thrown but std::exception caught with message: " + e.what(); \
+            QTest::qFail(msg.constData(), __FILE__, __LINE__);\
+            return;\
+        } QT_CATCH (...) {\
+            QTest::qFail("Expected exception of type " #exceptiontype " to be thrown" \
+                         " but unknown exception caught", __FILE__, __LINE__);\
+            return;\
+        }\
+    } while (0)
+
 class SqlHelper
 {
 public:
@@ -133,7 +158,7 @@ private Q_SLOTS :
       m_sqlHelper->insert(sql, QVariantList() << 2 << AnyBuilder::anyString() << AnyBuilder::anyString());
 
       auto session = m_sessionFactory->createSession();
-      QVERIFY_EXCEPTION_THROWN(session->selectOneBySql<PersonSimple>("select * from person"), MoreThanOneResultException);
+      VERIFY_EXCEPTION_THROWN(session->selectOneBySql<PersonSimple>("select * from person"), MoreThanOneResultException);
    }
 
    void selectOneBySql_onePersonWithNameOnlyIsSelected_onePersonWithNameOnlyIsFilled()
