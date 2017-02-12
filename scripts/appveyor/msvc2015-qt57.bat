72442623492e84d38525d58528f4bd7a604eb398
Marco Busemann <marco.busemann@live.com>
2017-02-12 11:08:20 +0100

    Updated ci scripts for travis and appveyor

--- scripts/appveyor/msvc2015-qt57.bat
+++ scripts/appveyor/msvc2015-qt57.bat
@@ -1,5 +1,8 @@
 set QTDIR=C:\Qt\5.7\msvc2015
 set PATH=%PATH%;%QTDIR%\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-qmake
+mkdir build
+cd build
+cmake ..
 nmake
+ctest -V
