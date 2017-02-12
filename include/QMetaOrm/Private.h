81e6b10c0ee5473f6298da3367f88774497e1ec4
Marco Busemann <marco.busemann@live.com>
2017-02-12 16:53:24 +0100

    Simplified and fixed symbol export for windows

--- include/QMetaOrm/Private.h
+++ include/QMetaOrm/Private.h
@@ -1,19 +1,8 @@
 #pragma once
 
-#ifdef Q_OS_WIN
-#  define QMETAORM_DECL_EXPORT     __declspec(dllexport)
-#  define QMETAORM_DECL_IMPORT     __declspec(dllimport)
-#elif defined(QT_VISIBILITY_AVAILABLE)
-#  define QMETAORM_DECL_EXPORT     __attribute__((visibility("default")))
-#  define QMETAORM_DECL_IMPORT     __attribute__((visibility("default")))
-#else
-#  define QMETAORM_DECL_EXPORT
-#  define QMETAORM_DECL_IMPORT
-#endif
-
 #ifdef QMETAORM_LIBRARY
-#  define QMETAORM_LIBRARY_API QMETAORM_DECL_EXPORT
+#  define QMETAORM_LIBRARY_API Q_DECL_EXPORT
 #else
-#  define QMETAORM_LIBRARY_API QMETAORM_DECL_IMPORT
+#  define QMETAORM_LIBRARY_API Q_DECL_IMPORT
 #endif
 
