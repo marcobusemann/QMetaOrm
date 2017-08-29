30177a3a9c6d83ebe0cd0ef6ade992433e081ecd
Kilian Helmenstein <kilian.helmenstein@treesoft.de>
2017-08-29 12:30:09 +0200

    Added additional cmake variable for specific include paths

--- test/cmake/base.cmake
+++ test/cmake/base.cmake
@@ -2,6 +2,7 @@ add_executable(${TESTNAME} ${SRC})
 target_link_libraries(${TESTNAME} QMetaOrm ${EXTRA_LIBS})
 target_compile_features(${TESTNAME} PRIVATE ${NEEDED_FEATURES})
 target_include_directories(${TESTNAME} PRIVATE
+							${EXTRA_INCLUDEPATHS}
                             ${CMAKE_CURRENT_BINARY_DIR}
                             ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
 
