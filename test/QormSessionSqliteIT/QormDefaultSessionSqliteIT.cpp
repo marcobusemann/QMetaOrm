ff242553c0ef0d5d9a4fcf9454beba87d25feefe
Kilian Helmenstein <kilian.helmenstein@treesoft.de>
2017-08-29 16:54:03 +0200

    Implemented abstract methods for Firebird database

--- test/QormSessionSqliteIT/QormDefaultSessionSqliteIT.cpp
+++ test/QormSessionSqliteIT/QormDefaultSessionSqliteIT.cpp
@@ -1,6 +1,69 @@
 #include "QormDefaultSessionIT.h"
 #include "DatabaseFactory.h"
 
+class SqlSqliteQueries : public SqlQueries
+{
+    QString selectOne() override
+    {
+        return QString("select * from person limit 1");
+    }
+
+    QString selectOne(const QStringList &aColumnNames) override
+    {
+        return QString("select %1 from person limit 1").arg(aColumnNames.join(";"));
+    }
+
+    QString selectOneWhereId() override
+    {
+        return QString("select * from person where id = ? limit 1");
+    }
+};
+
+class SqliteMappings : public Mappings
+{
+    QormMetaEntityBuilder personMappingWithSetupSourceAndSequence() override
+    {
+        static const auto builder = QormMetaEntityBuilder::anEntity()
+            .forSource("person");
+        return builder;
+    }
+
+    QormMetaEntity::Ptr personSimpleMapping() override
+    {
+        static const QormMetaEntity::Ptr map = QormMetaEntityBuilder::anEntity()
+            .forSource("person")
+            .withId(PersonSimple::p::id, "ID")
+            .withData(PersonSimple::p::name, "NAME")
+            .withData(PersonSimple::p::surname, "SURNAME")
+            .build<PersonSimple>();
+        return map;
+    }
+
+    QormMetaEntity::Ptr addressMapping() override
+    {
+        static const QormMetaEntity::Ptr map = QormMetaEntityBuilder::anEntity()
+            .forSource("address")
+            .withId(Address::p::id, "ID")
+            .withData(Address::p::country, "COUNTRY")
+            .withData(Address::p::postCode, "POSTCODE")
+            .withData(Address::p::street, "STREET")
+            .build<Address>();
+        return map;
+    }
+
+    QormMetaEntity::Ptr personComplexMapping() override
+    {
+        static const QormMetaEntity::Ptr map = QormMetaEntityBuilder::anEntity()
+            .forSource("person")
+            .withId(PersonComplex::p::id, "ID")
+            .withData(PersonComplex::p::name, "NAME")
+            .withData(PersonComplex::p::surname, "SURNAME")
+            .withOneToMany(PersonComplex::p::address, "ADDRESS", addressMapping())
+            .build<PersonComplex>();
+        return map;
+    }
+};
+
 class QormDefaultSessionSqliteIT : public QormDefaultSessionIT
 {
     Q_OBJECT
@@ -9,6 +72,16 @@ private:
     virtual QormDatabaseFactory::Ptr databaseFactory() const override
     {
         return SQLiteEmbeddedDatabaseFactory::factory();
+    }
+
+    virtual SqlQueries::Ptr sqlQueries() const override
+    {
+        return SqlQueries::Ptr(new SqlSqliteQueries());
+    }
+
+    virtual Mappings::Ptr mappings() const override
+    {
+        return Mappings::Ptr(new SqliteMappings());
     }
 };
 
