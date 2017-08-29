1b399435d85e09bd352bef7cbf09d09543bed992
Kilian Helmenstein <kilian.helmenstein@treesoft.de>
2017-08-29 12:39:40 +0200

    Adjusted cmake files to new test name; Specific QormSession integration tests now use shared test cases

---
+++ test/QormSessionSqliteIT/QormDefaultSessionSqliteIT.cpp
@@ -0,0 +1,23 @@
+﻿#include "QormDefaultSessionIT"
+#include "DatabaseFactory.h"
+
+class QormDefaultSessionSqliteIT : public QormDefaultSessionSqliteIT {
+    Q_OBJECT
+private:
+    QormSessionFactory::Ptr m_sessionFactory;
+    SqlHelper::Ptr m_sqlHelper;
+
+public:
+    QormDefaultSessionSqliteIT() { }
+
+private:
+
+    virtual QormDatabaseFactory databaseFactory() const override
+    {
+        return SQLiteEmbeddedDatabaseFactory::factory();
+    }
+};
+
+QTEST_APPLESS_MAIN(QormDefaultSessionSqliteIT)
+
+#include "QormDefaultSessionSqliteIT.moc"
