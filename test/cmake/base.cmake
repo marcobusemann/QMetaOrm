9bcf69f33e7899a70d69f3048d43ad0852b09946
Kilian Helmenstein <kilian.helmenstein@treesoft.de>
2017-08-31 15:20:19 +0200

    Fixed indentations

--- test/cmake/base.cmake
+++ test/cmake/base.cmake
@@ -2,8 +2,8 @@ add_executable(${TESTNAME} ${SRC})
 target_link_libraries(${TESTNAME} QMetaOrm ${EXTRA_LIBS})
 target_compile_features(${TESTNAME} PRIVATE ${NEEDED_FEATURES})
 target_include_directories(${TESTNAME} PRIVATE
-							${EXTRA_INCLUDEPATHS}
-                            ${CMAKE_CURRENT_BINARY_DIR}
-                            ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
+                           ${EXTRA_INCLUDEPATHS}
+                           ${CMAKE_CURRENT_BINARY_DIR}
+                           ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
 
 add_test(${TESTNAME} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${TESTNAME})
